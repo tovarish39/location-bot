7146241a5d9b846101c3f043266cb15f076afdfe
Alexey3934 <gorrus39@mial.ru>
2023-06-23 16:09:46 +0200

    q

--- start.rb
+++ start.rb
@@ -8,8 +8,16 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case 
     when !message.location.nil?
-      puts message.location.inspect
-      puts
+      # puts message.location.inspect
+      if message.location.live_period.nil?
+        answer = 'бот перестал получать отслеживание'
+        bot.api.send_message(chat_id: message.chat.id, text:answer)
+      else
+        answer = "Бот получил изменение вашего места положения. Точность отслеживания #{horizontal_accuracy} метра"
+        bot.api.send_message(chat_id: message.chat.id, text:answer)
+      end
+
+      # puts
     when message.text
       answer = 'отправьте боту место положение, выберите период отслеживания вашего места положения'
       bot.api.send_message(chat_id: message.chat.id, text:answer)
